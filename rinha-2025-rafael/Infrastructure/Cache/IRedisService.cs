681c25bed8e3111f23a20df74580e63c6580a260
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-08-11 23:37:51 -0300

    feat: cache service for redis

---
+++ rinha-2025-rafael/Infrastructure/Cache/IRedisService.cs
@@ -0,0 +1,9 @@
+﻿using rinha_2025_rafael.Domain;
+
+namespace rinha_2025_rafael.Infrastructure.Cache
+{
+    public interface IRedisService
+    {
+        Task EnqueuePaymentAsync(PaymentRequest request);
+    }
+}
---
+++ rinha-2025-rafael/Infrastructure/Cache/RedisService.cs
@@ -0,0 +1,25 @@
+﻿using rinha_2025_rafael.Domain;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace rinha_2025_rafael.Infrastructure.Cache
+{
+    public class RedisService : IRedisService
+    {
+        private readonly IDatabase _db;
+        private const string PaymentQueueKey = "payments_queue";
+
+        public RedisService(IConnectionMultiplexer redis)
+        {
+            _db = redis.GetDatabase();
+        }
+
+        public async Task EnqueuePaymentAsync(PaymentRequest request)
+        {
+            var payload = JsonSerializer.Serialize(request);
+
+            // Adiciona elemento no início (à esquerda) para o Worker pegar o do final (à direita).
+            await _db.ListLeftPushAsync(PaymentQueueKey, payload);
+        }
+    }
+}
