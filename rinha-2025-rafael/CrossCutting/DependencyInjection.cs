c5aca6ecff703d56231f1e2f0cb0b923d9d7f79d
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-08-11 23:38:17 -0300

    feat: cross cutting layer

---
+++ rinha-2025-rafael/CrossCutting/DependencyInjection.cs
@@ -0,0 +1,49 @@
+﻿using rinha_2025_rafael.Application.EnqueuePaymentUseCase;
+using rinha_2025_rafael.Application.GetSummaryUseCase;
+using rinha_2025_rafael.Infrastructure.Cache;
+using rinha_2025_rafael.Workers;
+using StackExchange.Redis;
+
+namespace rinha_2025_rafael.CrossCutting
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services
+                .AddUseCases()
+                .AddRedis(configuration)
+                .AddWorker();
+
+            return services;
+        }
+
+        private static IServiceCollection AddUseCases(this IServiceCollection services)
+        {
+            services.AddScoped<IEnqueuePaymentUseCase, EnqueuePaymentUseCase>();
+            services.AddScoped<IGetSummaryUseCase, GetSummaryUseCase>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["Redis:ConnectionString"] ?? throw new ArgumentNullException("ConnectionString is missing");
+
+            services.AddSingleton<IRedisService, RedisService>();
+            services.AddSingleton<IConnectionMultiplexer>(
+                sp => ConnectionMultiplexer.Connect(connectionString));
+
+            return services;
+        }
+
+        private static IServiceCollection AddWorker(this IServiceCollection services)
+        {
+            services.AddScoped<PaymentProcessingWorker>();
+
+            return services;
+        }
+    }
+}
