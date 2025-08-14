3bc8687dc56353b91645ce4caf798c9192de80bd
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-08-14 16:27:11 -0300

    feat: adding context for payment summary and summary details

---
+++ rinha-2025-rafael/CrossCutting/JsonContext.cs
@@ -0,0 +1,17 @@
+﻿using rinha_2025_rafael.Domain;
+using System.Text.Json.Serialization;
+
+namespace rinha_2025_rafael.CrossCutting
+{
+    // Este atributo informa ao compilador para gerar o código de serialização para este contexto.
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, WriteIndented = false)]
+    // Adicionando um atributo [JsonSerializable] para CADA tipo que precisamos converter de/para JSON.
+    [JsonSerializable(typeof(PaymentRequest))]
+    [JsonSerializable(typeof(HealthCheckResponse))]
+    [JsonSerializable(typeof(PaymentProcessorPayload))]
+    [JsonSerializable(typeof(PaymentSummaryResponse))]
+    [JsonSerializable(typeof(SummaryDetails))]
+    public partial class JsonContext : JsonSerializerContext
+    {
+    }
+}
